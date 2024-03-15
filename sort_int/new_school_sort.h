e82eecfe0dfe08a284d2615cfe614540f2fb2259
Davsnick <70839783+Davsnick@users.noreply.github.com>
2024-03-15 16:03:34 +0100

    Delete sort_int directory

--- sort_int/new_school_sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-// new_school_sort.h
-
-#ifndef NEW_SCHOOL_SORT_H
-#define NEW_SCHOOL_SORT_H
-
-int new_school_sort(char* infile, char* outfile);
-
-#endif /* NEW_SCHOOL_SORT_H */
--- sort_int/old_school_sort.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-// old_school_sort.h
-
-#ifndef OLD_SCHOOL_SORT_H
-#define OLD_SCHOOL_SORT_H
-
-int old_school_sort(char* infile, char* outfile);
-
-#endif /* OLD_SCHOOL_SORT_H */
